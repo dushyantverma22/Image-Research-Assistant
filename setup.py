70eed2d97c5e8deac9b2bcab49003a1c94bf90a0
Dushyant kumar verma <91840332+dushyantver@users.noreply.github.com>
2026-01-11 22:08:02 +0530

    packaging is done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Image assistant",
+    version="0.0.1",
+    author="Dushyant Verma",
+    author_email="dushyantdchss@gmail.com",
+    packages=find_packages()
+)
